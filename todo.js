5536e49c0cdb36dd6acc9cd7392f62b512dc342f
David Coven <covendavid94@gmail.com>
2014-06-24 02:48:38 -0700

    initial commit

---
+++ todo.js
@@ -0,0 +1,22 @@
+function TodoCtrl($scope) {
+    $scope.totalTodos =4;
+    
+    $scope.todos =[{text: 'Learn AngularJS', done:false}, {text: 'Build an App', done:false}];
+    
+   
+$scope.getTotalTodos = function () {
+   return $scope.todos.length;
+}
+
+ $scope.clearCompleted = function () {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+    };
+    
+$scope.addTodo = function () {
+    $scope.todos.push({text:$scope.formTodoText, done:false});
+    $scope.formTodoText = "";
+    
+    }
+}
